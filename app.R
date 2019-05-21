64e678d10fe4b34ed07571b4d3f590048591c0d8
jamesha95 <42016452+jamesha95@users.noreply.github.com>
2019-05-21 21:03:53 +1000

    Updated layout aesthetics

--- app.R
+++ app.R
@@ -6,12 +6,14 @@
 library(shiny)
 library(tidyverse)
 library(rcrossref)
-library(plotly)
+#library(plotly)
 library(shinyWidgets)
 library(countrycode)
 library(scales)
 library(zoo)
 library(lubridate)
+library(RColorBrewer)
+
 
 options(stringsAsFactors = FALSE)
 
@@ -48,7 +50,8 @@ all_data <- metrics_data %>%
                              measure_id == "https://metrics.operas-eu.org/obp-pdf/sessions/v1"  ~ "sessions", 
                              measure_id == "https://metrics.operas-eu.org/obp/downloads/v1" ~ "downloads",
                              measure_id == "https://metrics.operas-eu.org/open-edition/downloads/v1" ~ "downloads",
-                             measure_id == "https://metrics.operas-eu.org/world-reader/users/v1" ~ "users"))
+                             measure_id == "https://metrics.operas-eu.org/world-reader/users/v1" ~ "users")) %>% 
+  mutate(yq = as.yearqtr(timestamp.x)) # We add year-quarters for the readership dates
 # Have some data pre-processed and ready to go here, so that the entire datafile doesn't need uploading
 # each time someone uses the app. 
 titles <- all_data %>%
@@ -69,33 +72,37 @@ measures <- all_data %>%
 ui <- fluidPage(theme = "journal.css",
                 
                 # Application title
-                titlePanel("Readership metrics"),
+                titlePanel("HIRMEOS: Readership metrics"),
                 
                 # re-arrange the elements of the dashboard
+                fluidRow(
+                  # things the user can interact with
+                  wellPanel(pickerInput(inputId = "title", 
+                                        label = "Choose a title or select all", 
+                                        selected = titles,
+                                        choices = titles,
+                                        options = list(`actions-box` = TRUE),
+                                        multiple = T))
+                ),
                 
-                # a metric insensitive to title choice
-                textOutput(outputId = "no_titles"), # consider using h2 and choosing a different font
+                fluidRow(column(4,
+                                textOutput(outputId = "no_titles"), 
+                                textOutput("total_access"),
+                                textOutput("no_countries_reached")),
+                         
+                         column(4, tableOutput("metrics_table"))), #table of access by different metrics
                 
-                # things the user can interact with
-                pickerInput(inputId = "title", 
-                            label = "Choose a title or select all", 
-                            selected = titles,
-                            choices = titles,
-                            options = list(`actions-box` = TRUE),
-                            multiple = T),
-                
-                textOutput("total_access"),
-                textOutput("no_countries_reached"),
-                
-                tableOutput("metrics_table"), #table of access by different metrics
-                
-                pickerInput(inputId = "metric", 
-                            label = "Choose a metric or select all", 
-                            selected = measures,
-                            choices = measures,
-                            options = list(`actions-box` = TRUE),
-                            multiple = T),
-                plotOutput("monthly_access"),
+                wellPanel(checkboxGroupInput(inputId = "metric", 
+                                             label = "Choose metrics", 
+                                             selected = measures,
+                                             choices = measures),
+                          pickerInput(inputId = "title2", 
+                                      label = "Choose a title or select all", 
+                                      selected = titles,
+                                      choices = titles,
+                                      options = list(`actions-box` = TRUE),
+                                      multiple = T)),
+                plotOutput("monthly_access")
                 
                 
                 #global dot map
@@ -104,25 +111,25 @@ ui <- fluidPage(theme = "journal.css",
                 
                 
                 # some JS to recorc the window size
-                tags$head(tags$script('
-                                      var width = 0;
-                                      $(document).on("shiny:connected", function(e) {
-                                      width = window.innerWidth;
-                                      Shiny.onInputChange("width", width);
-                                      });
-                                      $(window).resize(function(e) {
-                                      width = window.innerWidth;
-                                      Shiny.onInputChange("width", width);
-                                      });
-                                      '))
+                # tags$head(tags$script('
+                #                       var width = 0;
+                #                       $(document).on("shiny:connected", function(e) {
+                #                       width = window.innerWidth;
+                #                       Shiny.onInputChange("width", width);
+                #                       });
+                #                       $(window).resize(function(e) {
+                #                       width = window.innerWidth;
+                #                       Shiny.onInputChange("width", width);
+                #                       });
+                #                       '))
                 
-                )
+)
 
 
 ##---- The Server -------------------------------------------------------------------------------
 server <- function(input, output) {
   output$no_titles <- renderText({
-    no_titles <- length(titles)
+    no_titles <- prettyNum(length(titles), big.mark = ",")
     return(paste0("Titles published: ", no_titles))
   })
   
@@ -130,7 +137,8 @@ server <- function(input, output) {
     total_access <- all_data %>%
       filter(title %in% input$title, !is.na(value.x)) %>% 
       pull(value.x) %>% 
-      sum()
+      sum() %>% 
+      prettyNum(big.mark = ",")
     return(paste0("Total acccess: ", total_access))
   })
   
@@ -147,27 +155,33 @@ server <- function(input, output) {
     this_data <- all_data %>%
       filter(title %in% input$title, !is.na(value.x)) %>%   
       group_by(measure)%>%
-      summarise(values = as.integer(sum(value.x))) 
+      summarise(value = prettyNum(sum(value.x),
+                                big.mark = ",")) %>%
+      rename(metric = measure)
     return(this_data)
-    
-  })
+  }, align = "cr", digits = 0)
   
   output$monthly_access <- renderPlot({
     this_data <- all_data %>%
-      filter(title %in% input$title) %>% 
+      filter(title %in% input$title2) %>% 
       filter(measure %in% input$metric) %>% 
-      mutate(yq = as.yearqtr(timestamp.x)) %>% 
       group_by(measure, yq)%>%
       summarise(values = sum(value.x)) %>%
-      arrange(yq)
+      arrange(yq, measure) %>% 
+      rename(metric = measure)
     
     p <- ggplot(this_data, mapping = aes(x = yq, y = values))
-    p <- p + geom_col(aes(fill = measure))
+    p <- p + geom_col(aes(fill = metric))
+    p <- p + scale_fill_manual(values = c("downloads" = brewer.pal(8, "YlGnBu")[5],
+                                          "sessions" = brewer.pal(8, "YlGnBu")[6],
+                                          "users" = brewer.pal(8, "YlGnBu")[7], 
+                                          "views" = brewer.pal(8, "YlGnBu")[8]))
     p <- p + scale_y_continuous(labels = comma)
     p <- p + ggtitle("Readership over time")
     p <- p + ylab("Interactions")
     p <- p + xlab("")
     p <- p + scale_x_yearqtr(format = "%Y-Q%q")
+    p <- p + theme_minimal()
     
     return(p) #consider return(ggplotly(p)) for interactivity, if we can get it to work
     # might need an API to publish plotly charts
